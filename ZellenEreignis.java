6b27f93f01e86cba68ab57d14371d059fb43af0a
lütje <luetje.helzel@gmail.com>
2025-11-21 11:01:21 +0100

    changes

--- ZellenEreignis.java
+++ ZellenEreignis.java
@@ -1,46 +1,35 @@
 
-
 /**
- * Beschreibt ein Ereignis für die Zellen.
- * Klicks auf die Zellen werden hier verarbeitet.
+ *
+ * Klasse ZellenEreignis
  * 
- * @author (Ihr Name)
- * @version 1.0
  */
-/**
- * Beschreibt ein Ereignis für die Karten.
- * Klicks auf die Karten werden hier verarbeitet.
- * 
- * @author (Ihr Name)
- * @version 1.0
- */
-public class ZellenEreignis extends Ereignisbehandlung
-{
-    // Array mit allen Zellen
-    Zelle[][] zellen;
+public class ZellenEreignis extends Ereignisbehandlung {
+  // Array mit allen Zellen
+  Zelle[][] zellen;
 
-    // Konstruktor
-    ZellenEreignis(Zelle[][] zellenArray) {
+  /**
+    konstruktor für das ZellenEreignis
+   */
+    ZellenEreignignis(Zelle[][] zellenArray) {
         this.zellen = zellenArray;
         Starten(); // Taktgeber starten
     }
 
-    // Überschreibt die Maus-Klick-Funktion
-    @Override
-    void MausGeklickt(int x, int y, int anzahl) {
+  // Überschreibt die Maus-Klick-Funktion
+  @Override
+  void MausGeklickt(int x, int y, int anzahl) {
     for (int i = 0; i < zellen.length; i++) {
-        for (int j = 0; j < zellen[i].length; j++) {
-            Zelle z = zellen[i][j];
+      for (int j = 0; j < zellen[i].length; j++) {
+        Zelle z = zellen[i][j];
 
-            if (x >= z.getX() && x <= z.getX() + z.getBreite() &&
-                y >= z.getY() && y <= z.getY() + z.getHöhe())
-            {
-                System.out.println("Zelle [" + i + "," + j + "] wurde angeklickt!");
-                
-                z.Aufdecken();  // ✔ richtig
-            }
+        if (x >= z.getX() && x <= z.getX() + z.getBreite() &&
+            y >= z.getY() && y <= z.getY() + z.getHöhe()) {
+          System.out.println("Zelle [" + i + "," + j + "] wurde angeklickt!");
+
+          z.Aufdecken();
         }
+      }
     }
+  }
 }
-}
-
