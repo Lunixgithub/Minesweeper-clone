a27c891766ea80cd1b41563ebe080bac13cc8676
lütje <luetje.helzel@gmail.com>
2025-12-02 21:14:52 +0100

    status functionality for victory

--- MinesweeperEreignis.java
+++ MinesweeperEreignis.java
@@ -1,16 +1,31 @@
 class MinesweeperEreignis extends Ereignisbehandlung {
-    
-    Zelle[][] zellen;
-    Spielfeld spielfeldRef; // Referenz auf das Spielfeld
 
-    MinesweeperEreignis(Zelle[][] zellenArray, Spielfeld sf) {
+    boolean flagMode = false;
+    boolean spielVorbei = false;
+
+    Zelle[][] zellen;
+    Spielfeld spielfeldRef;
+    SpielStatus status;   
+
+    MinesweeperEreignis(Zelle[][] zellenArray, Spielfeld sf, SpielStatus st) {
         this.zellen = zellenArray;
         this.spielfeldRef = sf;
-        Starten(); // Taktgeber starten
+        this.status = st;
+        Starten();
+    }
+
+    @Override
+    void TasteGedrückt(char taste) {
+        if (taste == 'f' || taste == 'F') {
+            flagMode = !flagMode;
+            System.out.println("Flag-Modus: " + (flagMode ? "AN" : "AUS"));
+        }
     }
 
     @Override
     void MausGeklickt(int x, int y, int anzahl) {
+
+        if (spielVorbei) return;
 
         for (int r = 0; r < zellen.length; r++) {
             for (int c = 0; c < zellen[r].length; c++) {
@@ -20,17 +35,50 @@ class MinesweeperEreignis extends Ereignisbehandlung {
                 if (x >= z.getX() && x <= z.getX() + z.getBreite() &&
                     y >= z.getY() && y <= z.getY() + z.getHöhe())
                 {
-                    if (z.istBombe) {
-                        z.Aufdecken();
-                        System.out.println("BOOM!");
+                    //flagmode
+                    if (flagMode) {
+                        z.toggleFlagge();
+                        status.flaggeGeändert(z);   // status informieren
                         return;
                     }
 
+                    
+                    if (z.istBombe) {
+                        z.Aufdecken();
+                        status.bombeGetroffen();
+
+                        // ALLE Bomben sichtbar machen
+                        for (int i = 0; i < zellen.length; i++) {
+                            for (int j = 0; j < zellen[i].length; j++) {
+                                if (zellen[i][j].istBombe)
+                                    zellen[i][j].Aufdecken();
+                            }
+                        }
+
+                        spielVorbei = true;
+                        System.out.println("BOOM! Game Over!");
+                        return;
+                    }
+
+                    //aufdecken normal
                     if (!z.istAufgedeckt) {
                         if (z.bombeNachbarn == 0)
                             spielfeldRef.floodFill(r, c);
                         else
                             z.Aufdecken();
+
+                        status.feldAufgedeckt();
+                    }
+
+                   //Siegprüfung
+                    if (status.flaggenGewonnen()) {
+                        System.out.println("GEWONNEN – alle Bomben markiert!");
+                        spielVorbei = true;
+                    }
+
+                    if (status.alleSicherenAufgedeckt()) {
+                        System.out.println("GEWONNEN – alle sicheren Felder aufgedeckt!");
+                        spielVorbei = true;
                     }
 
                     return;
@@ -39,3 +87,4 @@ class MinesweeperEreignis extends Ereignisbehandlung {
         }
     }
 }
+
